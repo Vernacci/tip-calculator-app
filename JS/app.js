4ba0512826fe16726da862e904030087d02a6b4a
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-06-17 10:42:31 -0300

    Updated js

---
+++ JS/app.js
@@ -0,0 +1,15 @@
+const buttons = document.querySelectorAll('.input-btn');
+const billInput = document.querySelector('#bill-js');
+const peopleInput = document.querySelector('#people-js');
+const customInput = document.querySelector('.custom');
+const resetBtn = document.querySelector('.reset-btn');
+const resultPeople = document.querySelector('#result-people');
+const resultBill = document.querySelector('#result-bill');
+
+buttons.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const result = (billInput.value * e.target.value) / peopleInput.value;
+    resultPeople.textContent = `$${result.toFixed(2)}`;
+    console.log(btn.value);
+  });
+});
