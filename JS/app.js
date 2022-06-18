748aa173c04d52538b43ef12d386294fe1a1e757
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-06-18 19:33:33 -0300

    yes

--- JS/app.js
+++ JS/app.js
@@ -7,8 +7,14 @@ const resultPeople = document.querySelector('#result-people');
 const resultBill = document.querySelector('#result-bill');
 
 buttons.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
+  btn.addEventListener('click', () => {
     const result = (billInput.value * btn.value) / peopleInput.value;
     resultPeople.textContent = `$${result.toFixed(2)}`;
   });
+});
+
+customInput.addEventListener('click', () => {
+  let customValue = customInput.value / 100 + 1;
+  const customResult = (billInput.value * customValue) / peopleInput.value;
+  resultPeople.textContent = `$${customResult.toFixed(2)}`;
 });
